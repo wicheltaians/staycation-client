0b774d184dddf12b16ecebbf6bb68bb9d23de3a3
Wichelta Iansyah <wicheltaiansyah078@gmail.com>
2023-05-06 02:32:59 +0700

    delete route for example component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from "pages/LandingPage";
 import DetailsPage from "pages/DetailsPage";
 import CheckoutPage from "pages/CheckoutPage";
-import InputNumber from "pages/ExampleInputNumber";
-import InputDate from "pages/ExampleInputDate";
-import Breadcrumb from "pages/ExampleBreadcrumb";
 import "assets/scss/style.scss";
 
 function App() {
@@ -15,9 +12,6 @@ function App() {
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/properties/:id" component={DetailsPage} />
         <Route path="/checkout" component={CheckoutPage} />
-        <Route path="/exampleinputnumber" component={InputNumber} />
-        <Route path="/exampleinputdate" component={InputDate} />
-        <Route path="/examplebreadcrumb" component={Breadcrumb} />
       </Router>
     </div>
   );
